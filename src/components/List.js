bf6b7d38f517cfb17a4ab90284914d49cbbf267a
sarangi-ankit <sarangiankit097@gmail.com>
2021-08-12 17:29:02 +0530

    updated

--- src/components/List.js
+++ src/components/List.js
@@ -1,125 +1,126 @@
-import React, { useState,useEffect } from 'react'
-import DeleteIcon from '@material-ui/icons/Delete';
-import EditIcon from '@material-ui/icons/Edit';
+import React, { useState, useEffect } from "react";
+import DeleteIcon from "@material-ui/icons/Delete";
+import EditIcon from "@material-ui/icons/Edit";
 
-const getLocalItems=()=>{
-    let list=localStorage.getItem("lists")
+const getLocalItems = () => {
+  let list = localStorage.getItem("lists");
+  if (list) {
+    return JSON.parse(localStorage.getItem("lists"));
+  } else {
+    return [];
+  }
+};
 
-    if (list){
-        return JSON.parse(localStorage.getItem("lists"))
+const List = () => {
+  const [todo, setTodo] = useState("");
+  const [items, setItems] = useState(getLocalItems());
+  const [toggle, setToggle] = useState(true);
+  const [isEdit, setIsEdit] = useState(null);
+
+  const addSubmit = (e) => {
+    setTodo(e.target.value);
+  };
+
+  const addKeyPress = (e) => {
+    if (e.key === "Enter") {
+
+      if (!todo) return;
+
+      else if (todo && !toggle) {
+        const updatedList = items.map((item) => {
+          if (item.id === isEdit) return { ...item, name: todo };
+          return item;
+        });
+        setItems(updatedList);
+        setToggle(true);
+        setTodo("");
+        setIsEdit(null);
+      } else {
+        const taskName = {
+          id: new Date().getTime().toString(),
+          name: todo,
+        };
+        setItems([taskName, ...items]);
+        setTodo("");
+      }
     }
-    else{
-        return []
+  };
+  const addBtn = () => {
+    if (!todo) return alert("Please add a task!");
+
+    else if (todo && !toggle) {
+      const updatedList = items.map((item) => {
+        if (item.id === isEdit) return { ...item, name: todo };
+        return item;
+      });
+
+      setItems(updatedList);
+      setToggle(true);
+      setTodo("");
+      setIsEdit(null);
+    } else {
+      const taskName = { id: new Date().getTime().toString(), name: todo };
+      setItems([taskName, ...items]);
+      setTodo("");
     }
-}
+  };
 
+  const deleteItems = (index) => {
+    const newList = items.filter((item)=>index !== item.id);
+    setItems(newList);
+  };
 
-export default function List() {
+  const editItems = (id) => {
+    const editItem = items.find((item)=>id === item.id);
+    setToggle(false);
+    setTodo(editItem.name);
+    setIsEdit(id);
+  };
 
-    const [list,setList]=useState("")
-    const [items,setItems]=useState(getLocalItems())
-    const [toggle,setToggle]=useState(true)
-    const [isEdit,setIsEdit]=useState(null)
+  useEffect(() => {
+    localStorage.setItem("lists", JSON.stringify(items));
+  }, [items]);
 
-    const addSubmit=(e)=>{
-        setList(e.target.value)
-        
-    }
+  return (
+    <div className="container">
+      <div className="box">
+        <h1 className="heading">Todo list</h1>
+        <input
+          type="text"
+          placeholder="Enter Your Task"
+          value={todo}
+          onKeyDown={addKeyPress}
+          onChange={addSubmit}
+        />
+        {toggle ? (
+          <button onClick={addBtn}>+</button>
+        ) : (
+          <button>
+            <EditIcon onClick={addBtn} />
+          </button>
+        )}
 
-    const addKeyPress=(e)=>{
-        
-        if (e.key === 'Enter') {
-            if(!list){
-
-            }else{
-                const allInputData={id:new Date().getTime().toString(),name:list}
-                setItems([...items,allInputData])
-                setList("")
-            }
-        }
-    }
-    const addBtn=()=>{
-        if(!list){
-            alert("plz fill the data")
-        }else if(list && !toggle){
-            setItems(
-                items.map((item)=>{
-                    if (item.id===isEdit){
-                        return {...item,name:list}
-                    }
-                    return item
-                }
-            ))
-            setToggle(true)
-            setList('')
-            setIsEdit(null) 
-        }
-        else{
-            const allInputData={id:new Date().getTime().toString(),name:list}
-            setItems([...items,allInputData])
-            setList("")
-        }
-    }
-
-    const deleteItems=(index)=>{
-        setItems((allData)=>{
-            return allData.filter((itemval)=>{
-                return index!==itemval.id
-            })
-        })
-    }
-    const editItems=(id)=>{
-        const editItem=items.find((elem)=>{
-            return id===elem.id
-        })
-        console.log(editItem)
-
-        setToggle(false)
-        setList(editItem.name)
-        setIsEdit(id)
-    }
-    
-
-    useEffect(() => {
-        
-        localStorage.setItem("lists",JSON.stringify(items))
-    }, [items])
- 
-    
-    return (
-        <div className="container">
-            <div className="sub-container">
-                <h1 className="heading">Todo list</h1>
-                <input 
-                type="text" 
-                placeholder="enter list" 
-                value={list}
-                onKeyDown={addKeyPress} 
-                onChange={addSubmit}
+        <ol className="sub-container">
+          {items.map((item) => {
+              const {id, name} = item;
+            return (
+              <div className="todo" key={id}>
+                <DeleteIcon
+                  className="deleteIcon icon"
+                  onClick={() => deleteItems(id)}
                 />
-                {toggle?<button onClick={addBtn}>+</button>:
-                <button><EditIcon onClick={addBtn} /></button>}
-                 
-                
-                <ol>
-                    {items.map((item)=>{
-                    return(
-                        <div key={item.id}>
-                            <span onClick={()=>deleteItems(item.id)}>
-                                <DeleteIcon className="deleteIcon" />
-                            </span>
-                            <span onClick={()=>editItems(item.id)}>
-                                <EditIcon className="deleteIcon" />
-                            </span>    
-                            <li>{item.name}</li>
-                            
-                        </div>
-                    ) 
-                        
-                    })}
-                    
-                </ol>
-            </div>
-        </div>
-    )
-}
+                <EditIcon
+                  className="deleteIcon icon"
+                  onClick={() => editItems(id)}
+                />
+                <li className="task-name">{name}</li>
+              </div>
+            );
+          })}
+        </ol>
+      </div>
+    </div>
+  );
+};
+
+export default List;
